07475dfb77b8e7bb6501d70e351e0373a425ae4b
Andrea Privitera <andrea.privitera@7gen.com>
2024-04-30 09:06:09 -0700

    Refs #5: Configured next for ssg

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
