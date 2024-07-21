96d406ec5746feaf957d0ddb130d09255e5767a6
Andrea Privitera <andrea.privitera@7gen.com>
2024-07-21 15:59:00 -0700

    Refs #25: Implemented tags page

---
+++ app/blog/tags/[tag]/data.ts
@@ -0,0 +1,20 @@
+import { readdirSync } from 'fs'
+
+export async function getTags () {
+  const fileList = readdirSync('./public/posts')
+  const tags: string[] = []
+  const checks = await Promise.all(
+    fileList.map(async (f) => {
+      const file = await import(`@/public/posts/${f}`)
+      return file.metadata.tags
+    })
+  )
+  checks.forEach((tagList) => {
+    tagList.forEach((tag: string) => {
+      if (!tags.includes(tag)) {
+        tags.push(tag)
+      }
+    })
+  })
+  return tags
+}
--- app/blog/tags/[tag]/page.tsx
+++ app/blog/tags/[tag]/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import { getTags } from './data'
+
+export async function generateStaticParams() {
+  const tags = await getTags()
+  return tags.map((tag) => ({ tag }))
+}
 
 export default function Page({ params }: { params: { tag: string } }) {
   const { tag } = params
   return (
-    <>
-      <h1>The tag is { tag }</h1>
-    </>
+    <Box sx={{ ml: 'auto', mr: 'auto', maxWidth: 850 }}>
+      <h1>Posts tagged <i>{ tag }</i></h1>
+    </Box>
   )
 }
