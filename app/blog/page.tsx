484d3f4f32ee523951c33e822fefb5ab3f143593
Andrea Privitera <andrea.privitera@7gen.com>
2024-07-20 22:16:02 -0700

    Refs #23: Blog posts organized by year

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,32 +1,49 @@
-import { Box, List, ListItemButton, ListItemText } from '@mui/material'
+import { Box, List, ListItemButton, ListItemText, Typography } from '@mui/material'
 import { getPostTitlesandSlugs } from './data'
 
 export default async function Page() {
   const posts = await getPostTitlesandSlugs()
+
+  const postsByYear = posts.reduce<Record<number, any[]>>((acc, post) => {
+    const year = new Date(post.date).getFullYear()
+    if (!acc[year]) {
+      acc[year] = []
+    }
+    acc[year].push(post)
+    return acc
+  }, {})
+
   return (
     <Box sx={{ ml: 'auto', mr: 'auto', maxWidth: 850 }}>
       <h1>Blog Posts</h1>
-      <List>
-        {
-        posts.map(({ title, date, slug }) => (
-          <ListItemButton key={slug}
-            component="a"
-            href={`/blog/${slug}`}
-          >
-            <ListItemText
-              primary={title}
-              secondary={
-                <time>
-                  {new Date(date).toLocaleDateString(
-                    'en-US', { dateStyle: 'long' }
-                  )}
-                </time>
-              }
-            />
-          </ListItemButton>
-        ))
-      }
-      </List>
+      {Object.keys(postsByYear).sort(
+        (a, b) => Number(b) - Number(a)).map((year) => (
+          <Box key={year}>
+            <h2>{year}</h2>
+            <List>
+              {postsByYear[Number(year)].map(
+                ({ title, date, slug }) => {
+                  const formattedDate = new Date(date).toLocaleDateString(
+                    'en-US', { day: 'numeric', month: 'long' }
+                  )
+                  return (
+                    <ListItemButton
+                      key={slug}
+                      component="a"
+                      href={`/blog/${slug}`}
+                    >
+                      <ListItemText
+                        primary={<Typography variant="h6">{title}</Typography>}
+                        secondary={<time>{formattedDate}</time>}
+                      />
+                    </ListItemButton>
+                  )
+                }
+              )}
+            </List>
+          </Box>
+      ))
+    }
     </Box>
   )
 }
