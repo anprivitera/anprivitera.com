a92e78cae4cc106a65f3cddba216e992cf6e674e
Andrea Privitera <andrea.privitera@7gen.com>
2024-07-21 21:00:00 -0700

    Refs #24: Removed unnecessary wrapping tags

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -92,56 +92,52 @@ export default function DrawerAppBar(props: { window?: () => Window }) {
     window !== undefined ? () => window().document.body : undefined
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar
-        component="nav"
-        position="static"
-        color="transparent"
-        sx={{ boxShadow: 'none' }}
-      >
-        <Toolbar sx={{ color: '#284178' }}>
-          <Typography
-            variant="h6"
-            component="a"
-            href="/"
-            sx={{ flexGrow: 1, color: 'inherit', ':hover': { color: '#AF5D63' } }}
-          >
-            Andrea Privitera
-          </Typography>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <Menu />
-          </IconButton>
-          <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
-            {appBarItems(navItems, false)}
-            <Divider orientation="vertical" flexItem />
-            {appBarItems(socialItems, true)}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          anchor="right"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
-          }}
+    <AppBar
+      component="nav"
+      position="static"
+      color="transparent"
+      sx={{ boxShadow: 'none' }}
+    >
+      <Toolbar sx={{ color: '#284178' }}>
+        <Typography
+          variant="h6"
+          component="a"
+          href="/"
+          sx={{ flexGrow: 1, color: 'inherit', ':hover': { color: '#AF5D63' } }}
         >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
+          Andrea Privitera
+        </Typography>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="end"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <Menu />
+        </IconButton>
+        <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+          {appBarItems(navItems, false)}
+          <Divider orientation="vertical" flexItem />
+          {appBarItems(socialItems, true)}
+        </Box>
+      </Toolbar>
+      <Drawer
+        container={container}
+        variant="temporary"
+        anchor="right"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+        }}
+      >
+        {drawer}
+      </Drawer>
+    </AppBar>
   )
 }
