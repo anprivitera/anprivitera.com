320cbe046c19dc31a595e9e2e3a1a97e39f6da54
Andrea Privitera <andrea.privitera@7gen.com>
2024-07-27 16:05:38 -0700

    Refs #58: Added 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+export default function NotFound() {
+  return (
+    <Box>
+      <h1>Page not found</h1>
+      <Typography variant="body1" sx={{ textAlign: 'center' }}>
+        The page you are looking for does not exist.
+      </Typography>
+    </Box>
+  )
+}
