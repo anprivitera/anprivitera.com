999ade6348b5f983e574a8b8c895f7350fa3cb00
Andrea Privitera <andrea.privitera@7gen.com>
2024-07-27 21:19:12 -0700

    Refs #67: Fixed footer moving on window resize

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <Divider sx={{ mt: '2%' }} />
-      <Typography sx={{ mt: '2%', mb: '2%', fontSize: '0.6em', textAlign: 'center' }}>
+      <Typography sx={{ mt: 2, mb: 2, fontSize: '0.6em', textAlign: 'center' }}>
         Created using NextJS, Material UI, Cloudflare.
         <br />
         This site does not collect your personal data.
